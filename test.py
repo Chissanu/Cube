c4ae910ca2abe521ca4a2e5235675cb980819816
785putt <mr.putty02@gmail.com>
2023-03-20 15:40:43 +0700

    fullscreen enabled

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from tkinter import *
-from tkinter import ttk
+# from tkinter import ttk
+import tkinter as tk
 
 class app:
     def __init__(self, master):
         self.master = master
-        self.master.geometry("400x400")
+        self.master.geometry("800x800")
         self.login()
     
     def login(self):
@@ -12,12 +13,12 @@ class app:
             i.destroy()
         self.frame1 = Frame(self.master, width=300, height=300)
         self.frame1.pack()
-        self.reg_txt = ttk.Label(self.frame1, text='login')
+        self.reg_txt = tk.Label(self.frame1, text='login', bg="blue", width="300", height="2")
         self.reg_txt.pack()
         
         # this will create a label widget
-        self.l1 = ttk.Label(self.frame1, text = "First:")
-        self.l2 = ttk.Label(self.frame1, text = "Second:")
+        self.l1 = tk.Label(self.frame1, text = "First:")
+        self.l2 = tk.Label(self.frame1, text = "Second:")
 
         self.l1.pack()
         self.l1_label = Entry(self.frame1, bd=5)
@@ -27,8 +28,8 @@ class app:
         self.l2_label = Entry(self.frame1, bd=5, show='*')
         self.l2_label.pack()
 
-        ttk.Label(self.frame1, text="").pack()
-        self.register_btn = ttk.Button(self.frame1, text="Go to Register", command=self.register)
+        tk.Label(self.frame1, text="").pack()
+        self.register_btn = tk.Button(self.frame1, text="Go to Register", command=self.register)
         self.register_btn.pack()
 
     
@@ -37,12 +38,14 @@ class app:
             i.destroy()
         self.frame2 = Frame(self.master, width=300, height=300)
         self.frame2.pack()
-        self.reg_txt2 = ttk.Label(self.frame2, text='register')
+        self.reg_txt2 = tk.Label(self.frame2, text='register')
         self.reg_txt2.pack()
-        ttk.Label(self.frame2, text="").pack()
-        self.login_btn = ttk.Button(self.frame2, text="Go to Login", command=self.login)
+        tk.Label(self.frame2, text="").pack()
+        self.login_btn = tk.Button(self.frame2, text="Go to Login", command=self.login)
         self.login_btn.pack()
 
-root = Tk()
-app(root)
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    root.attributes('-fullscreen', True)
+    app(root)
+    root.mainloop()
