919123f52ef1eb5ee0de9b437554784ee6e62c8f
Chissanu <chissanuoak2@gmail.com>
2023-03-29 22:39:48 +0700

    Format some variables and add function to exit program

--- Frontend/ui.py
+++ Frontend/ui.py
@@ -20,6 +20,7 @@ class app:
         self.master = master
         window_width = 1080
         window_height = 1920
+        master.bind('<Escape>',lambda e: quit(e))
         # get the screen dimension
         screen_width = self.master.winfo_screenwidth()
         screen_height = self.master.winfo_screenheight()
@@ -34,77 +35,80 @@ class app:
 
 
     def login(self):  
-        #   Setting up grid and frame for button widgets/ texts
-        #   comment these out for now, as they messed with the alignment of widgets for tkinter
-        # Grid.columnconfigure(root, index = 0, weight = 1)
-        # Grid.rowconfigure(root, 0, weight = 1)
-
+        """
+        Setting up grid and frame for button widgets/ texts
+        comment these out for now, as they messed with the alignment of widgets for tkinter
+        Grid.columnconfigure(root, index = 0, weight = 1)
+        Grid.rowconfigure(root, 0, weight = 1)
+        """
         for i in self.master.winfo_children():
             i.destroy()
-        #   Arrow on top corner left
+        # Arrow on top corner left
         self.arrow_logo = customtkinter.CTkImage(Image.open("Frontend\\logostorage\\material-symbols_arrow-back.png"), size=(50, 50))
         arrow_label = customtkinter.CTkButton(self.master, image=self.arrow_logo, text="", fg_color=BG_COLOR, command=self.main_menu)
         arrow_label.grid(row = 0, column = 0, sticky=tk.NW, columnspan=2)
 
-        #   Cube logo
-        self.imge = customtkinter.CTkImage(Image.open("Frontend\\logostorage\\vaadin_cube.png"), size=(180, 180))
-        img_label = customtkinter.CTkLabel(self.master, text="", image=self.imge)
+        # Cube logo
+        self.image = customtkinter.CTkImage(Image.open("Frontend\\logostorage\\vaadin_cube.png"), size=(180, 180))
+        img_label = customtkinter.CTkLabel(self.master, text="", image=self.image)
         img_label.grid(column=1, row=0, pady=35)
 
         tk.Label(self.master, text="Login", font=("Inter", 40), bg= BG_COLOR).grid(column=1, row=1, pady=5, sticky=tk.N)
 
-        #   Insert text widget/ To add in sending data to firebase admin things after actual login attempt
-        entry_1 = customtkinter.CTkEntry(self.master, placeholder_text="Username", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
-        entry_1.grid(column=1, row=2, sticky = N, pady=(50, 40))
+        # Insert text widget/ To add in sending data to firebase admin things after actual login attempt
+        username_entry = customtkinter.CTkEntry(self.master, placeholder_text="Username", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
+        username_entry.grid(column=1, row=2, sticky = N, pady=(50, 40))
 
         entry_2 = customtkinter.CTkEntry(self.master, placeholder_text="Password", show="*", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
         entry_2.grid(column=1, row=3, sticky = N)
 
-        #   Login button
-        bottom_ = customtkinter.CTkButton(self.master, text="Login", font=("Inter", 25), corner_radius=20, text_color=WHITE, fg_color=BUTTON, width=500, height=60, command=self.chat)
-        bottom_.grid(column=1, row=4, sticky = "s", pady=(200,100))
+        # Login button
+        log_btn = customtkinter.CTkButton(self.master, text="Login", font=("Inter", 25), corner_radius=20, text_color=WHITE, fg_color=BUTTON, width=500, height=60, command=self.chat)
+        log_btn.grid(column=1, row=4, sticky = "s", pady=(200,100))
 
 
     def register(self):  
-        #   Setting up grid and frame for button widgets/ texts
-        #   comment these out for now, as they messed with the alignment of widgets for tkinter
-        # Grid.columnconfigure(root, index = 0, weight = 1)
-        # Grid.rowconfigure(root, 0, weight = 1)
+        """
+        Setting up grid and frame for button widgets/ texts
+        comment these out for now, as they messed with the alignment of widgets for tkinter
+        Grid.columnconfigure(root, index = 0, weight = 1)
+        Grid.rowconfigure(root, 0, weight = 1)
+        """
 
         for i in self.master.winfo_children():
             i.destroy()
-        #   Arrow on top corner left
+        # Arrow on top corner left
         self.arrow_logo = customtkinter.CTkImage(Image.open("Frontend\\logostorage\\material-symbols_arrow-back.png"), size=(50, 50))
         arrow_label = customtkinter.CTkButton(self.master, image=self.arrow_logo, text="", fg_color=BG_COLOR, command=self.main_menu)
         arrow_label.grid(row = 0, column = 0, sticky=tk.NW, columnspan=2)
     
-        #   Cube logo
-        self.imge = customtkinter.CTkImage(Image.open("Frontend\\logostorage\\vaadin_cube.png"), size=(180, 180))
-        img_label = customtkinter.CTkLabel(self.master, text="", image=self.imge)
+        # Cube logo
+        self.image = customtkinter.CTkImage(Image.open("Frontend\\logostorage\\vaadin_cube.png"), size=(180, 180))
+        img_label = customtkinter.CTkLabel(self.master, text="", image=self.image)
         img_label.grid(column=1, row=0, pady=35)
 
         tk.Label(self.master, text="Register", font=("Inter", 40), bg=BG_COLOR).grid(column=1, row=1, pady=5)
 
-        #   Insert text widget/ To add in sending data to firebase admin things after actual login attempt
-        entry_1 = customtkinter.CTkEntry(self.master, placeholder_text="Name", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
-        entry_1.grid(column=1, row=2, pady=(50,20))
+        # Insert text widget/ To add in sending data to firebase admin things after actual login attempt
+        name_entry = customtkinter.CTkEntry(self.master, placeholder_text="Name", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
+        name_entry.grid(column=1, row=2, pady=(50,20))
 
-        entry_2 = customtkinter.CTkEntry(self.master, placeholder_text="Username", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
-        entry_2.grid(column=1, row=3)
+        username_entry = customtkinter.CTkEntry(self.master, placeholder_text="Username", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
+        username_entry.grid(column=1, row=3)
 
-        entry_3 = customtkinter.CTkEntry(self.master, placeholder_text="Password", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
-        entry_3.grid(column=1, row=4)
+        password_entry = customtkinter.CTkEntry(self.master, placeholder_text="Password", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
+        password_entry.grid(column=1, row=4)
 
-        entry_4 = customtkinter.CTkEntry(self.master, placeholder_text="Confirm password", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
-        entry_4.grid(column=1, row=5, pady=(15,0))
+        confirm_entry = customtkinter.CTkEntry(self.master, placeholder_text="Confirm password", font=("Inter", 20), corner_radius=15, text_color=GRAY, fg_color=WHITE, width=500, height=60)
+        confirm_entry.grid(column=1, row=5, pady=(15,0))
 
         #   Register button
-        bottom_ = customtkinter.CTkButton(self.master, text="Register", font=("Inter", 25), corner_radius=20, text_color=WHITE, fg_color=BUTTON, width=500, height=60, command=self.chat)
-        bottom_.grid(column=1, row=6, sticky = "s", pady=(200,100))
+        reg_btn = customtkinter.CTkButton(self.master, text="Register", font=("Inter", 25), corner_radius=20, text_color=WHITE, fg_color=BUTTON, width=500, height=60, command=self.chat)
+        reg_btn.grid(column=1, row=6, sticky = "s", pady=(200,100))
 
     def chat(self):  
-        #   Setting up grid and frame for button widgets/ texts
-        #   comment these out for now, as they messed with the alignment of widgets for tkinter
+        # Setting up grid and frame for button widgets/ texts
+        # comment these out for now, as they messed with the alignment of widgets for tkinter
         Grid.columnconfigure(root, index = 0, weight = 0)
         Grid.columnconfigure(root, index = 1, weight = 0)
         Grid.columnconfigure(root, index = 2, weight = 0)
@@ -132,9 +136,9 @@ class app:
         logout_label = customtkinter.CTkButton(sidebar_frame, image=logout_logo, text="", width=50, fg_color=BUTTON, command=self.main_menu)
         logout_label.grid(row = 2, column = 0, padx = 25, pady = (665, 25))
 
-        shutDown_logo = customtkinter.CTkImage(Image.open("Frontend\logostorage\ShutDown_btn.png"), size=(50, 50))
-        shutDown_label = customtkinter.CTkButton(sidebar_frame, image=shutDown_logo, text="", width=50, fg_color=BUTTON, command=root.destroy)
-        shutDown_label.grid(row = 3, column = 0, padx = 25, pady = (30, 25))
+        shutdown_logo = customtkinter.CTkImage(Image.open("Frontend\logostorage\ShutDown_btn.png"), size=(50, 50))
+        shutdown_label = customtkinter.CTkButton(sidebar_frame, image=shutdown_logo, text="", width=50, fg_color=BUTTON, command=root.destroy)
+        shutdown_label.grid(row = 3, column = 0, padx = 25, pady = (30, 25))
 
     def addFriend(self):
 
@@ -177,7 +181,7 @@ class app:
 
 
     def main_menu(self):
-        #   Setting up grid and frame for button widgets/ texts
+        # Setting up grid and frame for button widgets/ texts
         Grid.columnconfigure(root,0,weight=1)
         Grid.columnconfigure(root,1,weight=1)
         Grid.columnconfigure(root,2,weight=1)
@@ -186,25 +190,29 @@ class app:
 
         for i in self.master.winfo_children():
             i.destroy()
-        #   Title 
+        # Title 
         tk.Label(self.master, text="CUBE", font=("Inter", 64, "bold"), bg=BG_COLOR).grid(column=1, row=0, sticky=tk.N, padx=1, pady=45)
         
-        #   Cube logo
-        self.imge = customtkinter.CTkImage(Image.open("Frontend\\logostorage\\vaadin_cube.png"), size=(220, 220))
-        img_label = customtkinter.CTkLabel(self.master, text="", image=self.imge)
+        # Cube logo
+        self.image = customtkinter.CTkImage(Image.open("Frontend\\logostorage\\vaadin_cube.png"), size=(220, 220))
+        img_label = customtkinter.CTkLabel(self.master, text="", image=self.iamge)
         img_label.grid(column=1, row=1)
 
-        #   Menu texts/ three buttons: Login, Register, & Quit
+        # Menu texts/ three buttons: Login, Register, & Quit
         tk.Label(self.master, text="Welcome\nglad to see you!\n\n\n\n", font=("Inter", 25), bg=BG_COLOR).grid(column=1, row=2, padx=1, pady=10, rowspan=2, sticky=tk.N)
 
-        bt1 = customtkinter.CTkButton(self.master, text="Login", font=("Inter", 35), corner_radius=20, text_color=WHITE, fg_color=BUTTON, width=350, height=75, command=self.login)
-        bt1.grid(column=1, row=3, pady=100)
+        btn1 = customtkinter.CTkButton(self.master, text="Login", font=("Inter", 35), corner_radius=20, text_color=WHITE, fg_color=BUTTON, width=350, height=75, command=self.login)
+        btn1.grid(column=1, row=3, pady=100)
 
-        bt2 = customtkinter.CTkButton(self.master, text="Register", font=("Inter", 35), corner_radius=20, text_color=WHITE, fg_color=BUTTON, width=350, height=75, command=self.register)
-        bt2.grid(column=1, row=2, rowspan=2, sticky=tk.S, pady=30)
+        btn2 = customtkinter.CTkButton(self.master, text="Register", font=("Inter", 35), corner_radius=20, text_color=WHITE, fg_color=BUTTON, width=350, height=75, command=self.register)
+        btn2.grid(column=1, row=2, rowspan=2, sticky=tk.S, pady=30)
 
-        bt3 = customtkinter.CTkButton(self.master, text="Quit", font=("Inter", 35), corner_radius=20, text_color=GENERAL_TEXT, fg_color=WHITE, width=250, height=75, command=root.destroy)
-        bt3.grid(column=1, row=4)
+        btn3 = customtkinter.CTkButton(self.master, text="Quit", font=("Inter", 35), corner_radius=20, text_color=GENERAL_TEXT, fg_color=WHITE, width=250, height=75, command=root.destroy)
+        btn3.grid(column=1, row=4)
+        
+    def quit(self,e):
+        self.destroy()
+        
 
 # class FriendList(customtkinter.CTkScrollableFrame):
 #     def __init__(self, master, command=None, **kwargs):
