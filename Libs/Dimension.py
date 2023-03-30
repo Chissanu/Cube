d29a30dcdebb494a349c3d4ac252575b5f275b40
FiresoftGH <most.chuti@gmail.com>
2023-03-30 18:01:40 +0700

    Rewrite the codes into an OOP format and add more features

--- Libs/Dimension.py
+++ Libs/Dimension.py
@@ -1,30 +1,66 @@
+# Import the required libraries
 import cv2
 import torch
-# Capture the camera frame
-vid = cv2.VideoCapture("http://10.100.9.1:4747/mjpegfeedjjjj")
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=r"C:\Users\Firesoft\Documents\Computing\Testing_Grounds\trained_models\model_201.pt")
-fps = vid.get(cv2.CAP_PROP_FPS)
-print('frames per second =',fps)
-success, img = vid.read()
+import time
 
-# Emotion Count Dictionary
-emotion_count = {"happy": 0, "sad": 0, "neutral": 0, "angry": 0, "disgust": 0, "surprise": 0}
-fno = 0
-count = 0
+class Detect:
+	def __init__(self):
+		# This dictionary stores emotions count detected by the A.I
+		self.emotion_table = {"happy": 0, "sad": 0, "neutral": 0, "angry": 0, "disgust": 0, "surprise": 0}
+	
+	# Input the emotion and it returns how many times the A.I detected these emotions according to the emotion_table.
+	def getEmotionData(self, key=None):
+		if key:
+			return [key, self.emotion_table[key]]
+		
+		else:
+			return self.emotion_table
 
-while success:
-	if fno % 32 == 0:
-		results = model(img)
-		count +=1
-	try:
-		emotion_count[results.pandas().xyxy[0].name[0]] += 1
-		print(emotion_count)
-			    
-	except IndexError:
-		print("Not Detected")
-			
-	# print(count,results.pandas().xyxy[0])
-	# read next frame
-	success, img = vid.read()
+	# (WIP) Get the most occuring emotion at a given interval
+	def getMostOccuringEmotion(self):
+		operator = self.emotion_table
+		max_value = max(self.emotion_table, key=lambda x:operator[x])
+		return max_value
+	
+	# Takes three inputs, the source of the video (0 for webcam), the model_path and the duration. Returns nothing at the moment
+	def timedDetection(self, source, model_path, duration):
+		vid = cv2.VideoCapture(source)
+		model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
+		fno = 0
+		success, img = vid.read()
+		initial = time.time()
 
-print("total frames",count)
+		while success:
+	
+			if fno % 32 == 0:
+				results = model(img)
+
+			try:
+				self.emotion_table[results.pandas().xyxy[0].name[0]] += 1
+				current = time.time()
+				if current - initial >= duration:
+					print(current - initial)
+					break
+
+				print(self.emotion_table)
+
+			except IndexError:
+				print("Not Detected")
+				pass
+
+			# read next frame
+			success, img = vid.read()
+
+# Test run codes. Will be removed in the final iteration of this script.
+
+# test = Detect()
+# test.timedDetection("http://10.100.9.1:4747/mjpegfeed", "C:\\Users\\Firesoft\\Documents\\Computing\\Testing_Grounds\\trained_models\\model_201.pt", 5)
+
+# while True:
+# 	query = str(input("Choices: [None, data]?:" ))
+# 	if query == 'data':
+# 		print(test.getEmotionData())
+
+# 	else:
+# 		print(test.getMostOccuringEmotion())
+		
